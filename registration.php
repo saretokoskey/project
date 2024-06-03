a9a198af5841fcdc301e063526532ec47ed1ceb3
saretokoskey <koskeybeard0@gmail.com>
2024-06-03 15:36:12 +0300

    my commit

--- registration.php
+++ registration.php
@@ -12,6 +12,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
