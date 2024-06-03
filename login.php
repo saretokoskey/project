6be9d4f46b3af6a655aff22f2501ac9d8564def7
saretokoskey <koskeybeard0@gmail.com>
2024-06-03 15:41:43 +0300

    change 1

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Invalid password.";
         }
     } else {
-        echo "No user found with this username and email.";
+        echo "The user does not exist.";
 
     }
 }
